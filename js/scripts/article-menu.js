3bb0431ec10e131f7cd7f6eea45c8c593b79ff73
Shamanka FE <shamankafe@gmail.com>
2023-05-12 11:19:49 +0200

    Article-menu realized in JS

---
+++ js/scripts/article-menu.js
@@ -0,0 +1,32 @@
+const articleMenu = document.querySelector('#contents');
+const list = articleMenu.querySelector('.contents__list');
+const template = document.querySelector('#article-menu').content;
+const chapters = document.querySelectorAll('.chapter');
+console.log(chapters);
+
+const renderArticleMenu = () => {
+  if (!articleMenu) {
+    return;
+  }
+
+  const renderItemClone = (el) => {
+    const itemClone = template.cloneNode(true);
+
+    const itemLink = itemClone.querySelector('.contents__link');
+    itemLink.href = `#${el.id}`;
+    itemLink.textContent = el.firstElementChild.textContent;
+
+    return itemClone;
+  };
+
+  const fragment = document.createDocumentFragment();
+
+  for (let i = 0; i < chapters.length; i++) {
+    const newItem = renderItemClone(chapters[i]);
+    fragment.append(newItem);
+  }
+
+  list.append(fragment);
+};
+
+export { renderArticleMenu };
